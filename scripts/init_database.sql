06cbc31aa2c681bdce19d255594329a18bcb4a52
Emriqurrizal Yahya Nurramdahan <emriqurrizal@gmail.com>
2026-08-06 20:40:49 +0700

    update comments

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,10 +1,16 @@
+--Create a new database called DataWarehouse and three schemas: bronze, silver, and gold.
+
 USE master;
 GO
 
+--create db
 CREATE DATABASE DataWarehouse;
+GO
 
 USE DataWarehouse;
+GO
 
+--create schemas
 CREATE SCHEMA bronze;
 GO
 CREATE SCHEMA silver;
